b976deaaeaba1c40c37110dd6b88222368d08c49
felixgames097 <felixcrat@gmail.com>
2022-07-17 15:10:25 +0200

    Add Github Actions

---
+++ src/main/java/me/foreverealize/mods/extradebugging/DimensionLogger.java
@@ -0,0 +1,20 @@
+package me.foreverealize.mods.extradebugging;
+
+import net.minecraft.entity.EntityType;
+import net.minecraftforge.event.entity.EntityTravelToDimensionEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class DimensionLogger {
+
+    // Directly reference a log4j logger.
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    @SubscribeEvent
+    public static void onDimensionLoaded(EntityTravelToDimensionEvent event) {
+        if (event.getEntity().getType() == EntityType.PLAYER) {
+            LOGGER.warn("Player " + event.getEntity().getDisplayName().getString() + " with UUID " + event.getEntity().getUniqueID() + " entered dimension " + event.getDimension().getLocation());
+        }
+    }
+}
